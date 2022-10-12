bec1a8b86e934c65e195dbfa8515d5a516a336ea
Marat Ikhsanov <m.ihsanov2009@yandex.ru>
2022-10-12 12:51:34 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -13,5 +13,5 @@ router_v1.register(r'titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
