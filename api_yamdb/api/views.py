00dfd4b214e66337b2623530b4034002cf32552d
Marat Ikhsanov <m.ihsanov2009@yandex.ru>
2022-10-23 10:05:02 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -48,7 +48,9 @@ def signup(request):
         )
     except IntegrityError:
         return Response(
-            {'message': 'Пользователь с таким именем или почтой уже существует.'},
+            {
+                'message': 'Пользователь с таким именем или почтой уже существует.'
+            },
             status=status.HTTP_400_BAD_REQUEST
         )
     confirmation_code = default_token_generator.make_token(user)
